c7127adbdb5accfabdaf7aaf331a404522d0c13b
pautib <entimos34@gmail.com>
2024-08-26 15:34:21 +0200

    First changes

---
+++ src/Raffle.sol
@@ -0,0 +1,56 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+/**
+ * @title Raffle
+ * @author pautib
+ * @notice None
+ */
+contract Raffle is VRFConsumerBaseV2Plus {
+
+    /* Error */
+    error Raffle__NotEnoughEthToEnterRaffle();
+
+    uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!");
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthToEnterRaffle();
+        }
+
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEntered(msg.sender);
+
+    }
+
+    function pickWinner() external {
+        // check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+        // Get our random number to decide winner
+    }
+
+    /** Getter functions */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
